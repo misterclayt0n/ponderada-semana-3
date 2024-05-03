3140641d3e7e7509f793a3da61ba1b5eff2cf5a1
misterclayt0n <daviarantes13@gmail.com>
2024-05-03 14:35:17 -0300

    add more features

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ type Recinto struct {
 	EspeciesAnimais string
 	Animais         []*Animal
 	BemCuidado      bool
+	Visitantes      int
 }
 
 func NovoAnimal(nome, especie string, felicidade int) *Animal {
@@ -29,6 +30,14 @@ func NovoRecinto(especies string, bemCuidado bool) *Recinto {
 	}
 }
 
+func (a *Animal) String() string {
+	return fmt.Sprintf("Nome: %s, Especie: %s, Felicidade: %d", a.Nome, a.Especie, a.Felicidade)
+}
+
+func (r *Recinto) String() string {
+	return fmt.Sprintf("EspeciesAnimais: %s, Animais: %v, BemCuidado: %t, Visitantes: %d", r.EspeciesAnimais, r.Animais, r.BemCuidado, r.Visitantes)
+}
+
 func (r *Recinto) AdicionarAnimal(animal *Animal) {
 	if animal.Especie == r.EspeciesAnimais {
 		r.Animais = append(r.Animais, animal)
@@ -39,6 +48,25 @@ func (a *Animal) Alimentar() {
 	a.Felicidade += 10
 }
 
+func (r *Recinto) AtrairVisitantes() {
+	if r.BemCuidado && r.calcularFelicidadeMedia() >= 50 {
+		r.Visitantes = 10
+	} else {
+		r.Visitantes = 0
+	}
+}
+
+func (r *Recinto) calcularFelicidadeMedia() int {
+	if len(r.Animais) == 0 {
+		return 0
+	}
+	totalFelicidade := 0
+	for _, animal := range r.Animais {
+		totalFelicidade += animal.Felicidade
+	}
+	return totalFelicidade / len(r.Animais)
+}
+
 func main() {
-	fmt.Println("OOP em go chefe!")
+	fmt.Println("OOP em Go!")
 }
--- main_test.go
+++ main_test.go
@@ -29,3 +29,42 @@ func TestAlimentarAnimal(t *testing.T) {
 		t.Errorf("o nivel esperado de felicidade era 60, obtido: %d", animal.Felicidade)
 	}
 }
+
+func TestAtrairVisitantesBemCuidado(t *testing.T) {
+	recinto := NovoRecinto("Leão", true)
+	animal := NovoAnimal("Simba", "Leão", 60)
+	recinto.AdicionarAnimal(animal)
+	recinto.AtrairVisitantes()
+	if recinto.Visitantes != 10 {
+		t.Errorf("esperado 10 visitantes, recebido %d", recinto.Visitantes)
+	}
+}
+
+func TestAtrairVisitantesMalCuidado(t *testing.T) {
+	recinto := NovoRecinto("Leão", false)
+	animal := NovoAnimal("Simba", "Leão", 60)
+	recinto.AdicionarAnimal(animal)
+	recinto.AtrairVisitantes()
+	if recinto.Visitantes != 0 {
+		t.Errorf("esperado 0 visitantes, recebido %d", recinto.Visitantes)
+	}
+}
+
+func TestCalcularFelicidadeMediaVazia(t *testing.T) {
+	recinto := NovoRecinto("Leão", true)
+	if media := recinto.calcularFelicidadeMedia(); media != 0 {
+		t.Errorf("esperado 0 de felicidade média, recebido %d", media)
+	}
+}
+
+func TestCalcularFelicidadeMediaComAnimais(t *testing.T) {
+	recinto := NovoRecinto("Leão", true)
+	animal1 := NovoAnimal("Simba", "Leão", 40)
+	animal2 := NovoAnimal("Nala", "Leão", 60)
+	recinto.AdicionarAnimal(animal1)
+	recinto.AdicionarAnimal(animal2)
+	expectedMedia := 50
+	if media := recinto.calcularFelicidadeMedia(); media != expectedMedia {
+		t.Errorf("esperado %d de felicidade média, recebido %d", expectedMedia, media)
+	}
+}
